f408fd8d197fc0d12d92180a25181c5314c3f510
CreeperZayden <112803649+CreeperZayden@users.noreply.github.com>
2024-08-17 11:33:06 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('name.html')
+    return render_template('eaglecraft.1.8.8.html')
 
 def run():
     app.run(host='0.0.0.0', port=8080)
