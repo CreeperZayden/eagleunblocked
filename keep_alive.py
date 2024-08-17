da799bf3e3415ee496fd96f28b8f9e616e2310f7
CreeperZayden <112803649+CreeperZayden@users.noreply.github.com>
2024-08-17 12:02:50 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from threading import Thread
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -11,12 +10,6 @@ def home():
     return render_template('eaglecraft.1.8.8.html')
 
 # Function to run the Flask server
-def run():
-    # Run the app with debug mode enabled on port 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-# Function to keep the Flask server running in the background
 def keep_alive():
-    # Start the server in a new thread
-    t = Thread(target=run)
-    t.start()
+    # Run the app in the main thread
+    app.run(host='0.0.0.0', port=5000, debug=True)
